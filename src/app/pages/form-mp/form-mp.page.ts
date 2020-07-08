2178d1613ab7378498a2e20f01bd05ca685bd9f0
Your Name <estebangulias@hotmail.com>
2020-07-08 10:09:53 -0300

    commit 4

--- src/app/pages/form-mp/form-mp.page.ts
+++ src/app/pages/form-mp/form-mp.page.ts
@@ -15,13 +15,14 @@ declare var Mercadopago:any
 })
 export class FormMpPage implements OnInit {
   docType:any;
-
+  doSubmit:boolean = false
+  payment_method_id:string = '';
 
   @ViewChild('cardNumber', {static:true}) cardNumber:string
   @ViewChild('installments', {static:true}) installments:string;
-  @ViewChild('payment_method_id', {static:true}) payment_method_id:string;
+   
   @ViewChild('transaction_amount', {static:true}) transaction_amount:any;
-
+  @ViewChild('formulario', {static:true}) formulario:Object;
   constructor( ) { 
     
     console.log(typeof Mercadopago)
@@ -47,12 +48,19 @@ export class FormMpPage implements OnInit {
     console.log()
   }
 
-  guessPaymentMethod(event) {
-    console.log(this.cardNumber)
-    let cardnumber = this.cardNumber;
+  // cardValue(event){
+  //   console.log(event)
+  //   this.cardNumber = event.detail.value
+  // }
 
+  guessPaymentMethod(event) {
+    //console.log(event.detail.value)
+    //  this.cardNumber = event.detail.value
+    let cardnumber = event.detail.value;
+    console.log(cardnumber)
     if (cardnumber.length >= 6) {
         let bin = cardnumber.substring(0,6);
+        console.log(bin)
         Mercadopago.getPaymentMethod({
             "bin": bin
         }, this.setPaymentMethod);
@@ -61,32 +69,67 @@ export class FormMpPage implements OnInit {
 
   setPaymentMethod(status, response) {
     if (status == 200) {
-        let paymentMethodId = response[0].id;
-        let element = this.payment_method_id;
-        // element.value = paymentMethodId;
+      console.log(response)
+      console.log(this.payment_method_id)
+      this.payment_method_id = response[0].id;
+        //let element = this.payment_method_id;
+        
+        //console.log(paymentMethodId)
+        //this.payment_method_id = paymentMethodId;
+        console.log(this.payment_method_id)
         this.getInstallments();
     } else {
+        console.log(response)
         alert(`payment method info error: ${response}`);
     }
   }
 
   getInstallments(){
         Mercadopago.getInstallments({
+          
         "payment_method_id": this.payment_method_id,
         "amount": parseFloat(this.transaction_amount)
 
     }, function (status, response) {
         if (status == 200) {
+            console.log(response)
               this.installments.options.length = 0;
             response[0].payer_costs.forEach( installment => {
                 let opt = document.createElement('option');
                 opt.text = installment.recommended_message;
                 opt.value = installment.installments;
                 this.installments.appendChild(opt);
+                console.log(response)
             });
         } else {
             alert(`installments method info error: ${response}`);
         }
     });
   }
+
+  doPay(formulario){
+    
+     if(!this.doSubmit){
+        //var $form = document.querySelector('#pay');
+        console.log(formulario)
+        Mercadopago.createToken(formulario, this.sdkResponseHandler);
+
+        return false;
+     }
+  };
+
+  sdkResponseHandler(status, response) {
+    if (status != 200 && status != 201) {
+        alert("verify filled data");
+    }else{
+        var form = this.formulario
+        // var card = document.createElement('input');
+        // card.setAttribute('name', 'token');
+        // card.setAttribute('type', 'hidden');
+        // card.setAttribute('value', response.id);
+        // form.appendChild(card);
+        // doSubmit=true;
+        // form.submit();
+    }
+  };
 }
